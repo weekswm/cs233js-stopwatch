9c3ef8950dc027c7d6b2ef1185113e886f1533b4
goodmari <goodm@lanecc.edu>
2018-03-17 12:07:48 -0700

    Added starting files

---
+++ index.js
@@ -0,0 +1,51 @@
+/*
+    Create 3 global variables, isRunning, timer and timerTime.
+    Initialize them to false, null and 0 respectively.
+*/
+
+function init()
+{
+    // Put the element on the page with an id of start in a variable
+    // Do the same for the stop button and the reset button
+
+
+    // Add an onclick handler to each of the buttons
+    // Use the functions startTimer, stopTimer and resetTimer 
+
+}
+
+
+function startTimer() {
+    // if the timer is NOT running, start it
+    // call the function incrementTimer every second
+    // save the timer in a the timer variable
+
+}
+
+function incrementTimer() {
+    // increment the timerTime
+    // calculate the number of minutes and seconds
+
+    // write the minutes and seconds to the elements on the page
+    // use the function pad to make sure there's a leading 0 if necessary
+}
+
+function pad(number) {
+    // add a leading 0 if the number is < 10
+}
+
+function stopTimer() {
+    // if the timer is running, stop it
+
+}
+
+function resetTimer() {
+    // stop the timer
+    
+    // set the timerTime back to 0
+    
+    // write 00 to the elements on the page for minutes and seconds
+
+}
+
+// When the page has finished loading, call the function init
